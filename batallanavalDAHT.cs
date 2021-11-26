2388ba55abc60c18de3f289ce6b7764113a730c8
AlejandroSSJ12 <89933737+AlejandroSSJ12@users.noreply.github.com>
2021-11-25 20:54:17 -0600

    Update batallanavalDAHT.cs

--- batallanavalDAHT.cs
+++ batallanavalDAHT.cs
@@ -1,108 +1,105 @@
 using System;
 
 
-namespace BataallaNavalDAHT
+namespace NavalBattle
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string[] posicionBarcoJugador1 = new string[3];
-            string[] posicionBarcoJugador2 = new string[3];
+            string[] posBarco1 = new string[3];
+            string[] posBarco2 = new string[3];
 
-            string[,] coordenadaElegidaJugador1 = new string[1, 6];
+            string[,] coordenadaJugador1 = new string[1, 6];
 
-            string[,] coordenadaElegidaJugador2 = new string[1, 6];
+            string[,] coordenadaJugador2 = new string[1, 6];
 
             for (int i = 0; i < 1; i++)
             {
                 for (int n = 0; n < 6; n++)
                 {
-                    coordenadaElegidaJugador1[i, n] = "";
-                    coordenadaElegidaJugador2[i, n] = "";
+                    coordenadaJugador1[i, n] = "";
+                    coordenadaJugador2[i, n] = "";
                 }
             }
 
-            int vidasJugador1 = 3;
-            int vidasJugador2 = 3;
+            
 
-            mostrarCoordenadas(coordenadaElegidaJugador1, posicionBarcoJugador2, 1);
+            mostrarCoordenadas(coordenadaJugador1, posBarco2, 1);
             Console.WriteLine("Hola a el juego Batalla Naval. \nRecuerda que este es un juego de coordenadas y el tablero solo sera de 1*6");
             Console.WriteLine("JUGADOR 1");
             Console.WriteLine("Escriba la posicion de sus barcos");
             for (int i = 0; i < 3; i++)
             {
                 Console.Write("Barco " + (i + 1) + ": ");
-                posicionBarcoJugador1[i] = Console.ReadLine();
-                posicionBarcoJugador1[i] = posicionBarcoJugador1[i].ToUpper();
+                posBarco1[i] = Console.ReadLine();
+                posBarco1[i] = posBarco1[i].ToUpper();
             }
 
             Console.Clear();
 
-            mostrarCoordenadas(coordenadaElegidaJugador2, posicionBarcoJugador1, 2);
+            mostrarCoordenadas(coordenadaJugador2, posBarco1, 2);
             Console.WriteLine("Hola a el juego Batalla Naval. \nRecuerda que este es un juego de coordenadas y el tablero solo sera de 1*6");
             Console.WriteLine("JUGADOR 2");
             Console.WriteLine("Escriba la posicion de sus barcos");
             for (int i = 0; i < 3; i++)
             {
                 Console.Write("Barco " + (i + 1) + ": ");
-                posicionBarcoJugador2[i] = Console.ReadLine();
-                posicionBarcoJugador2[i] = posicionBarcoJugador2[i].ToUpper();
+                posBarco2[i] = Console.ReadLine();
+                posBarco2[i] = posBarco2[i].ToUpper();
             }
 
             Console.Clear();
 
+            int vidas1 = 3;
+            int vidas2 = 3;
 
             for (int i = 0; i < 1; i++)
             {
                 for (int n = 0; n < 6; n++)
                 {
-                    //Turno primer jugador
-                    mostrarCoordenadas(coordenadaElegidaJugador1, posicionBarcoJugador2, 1);
+                    mostrarCoordenadas(coordenadaJugador1, posBarco2, 1);
                     Console.WriteLine("JUGADOR 1");
                     Console.Write("Escoja una coordenada: o");
                     
-                    coordenadaElegidaJugador1[i, n] = Console.ReadLine();
-                    coordenadaElegidaJugador1[i, n] = coordenadaElegidaJugador1[i, n].ToUpper();
+                    coordenadaJugador1[i, n] = Console.ReadLine();
+                    coordenadaJugador1[i, n] = coordenadaJugador1[i, n].ToUpper();
                     Console.WriteLine("Cuando escribas tu coordenada presiona ENTER");
                     for (int a = 0; a < 3; a++)
                     {
-                        if (posicionBarcoJugador2[a] == coordenadaElegidaJugador1[i, n])
+                        if (posBarco1[a] == coordenadaJugador1[i, n])
                         {
                             Console.WriteLine("Le diste a un barco");
-                            vidasJugador2 = vidasJugador2 - 1;
+                            vidas2 = vidas2 - 1;
                             Console.ReadKey();
                         }
                     }
 
                     Console.Clear();
-                    if (vidasJugador2 == 0)
+                    if (vidas2 == 0)
                     {
                         Console.WriteLine("JUGADOR 1 GANO");
                         Console.ReadKey();
                         return;
                     }
-
-
-                    //Turno segundo jugador
-                    mostrarCoordenadas(coordenadaElegidaJugador2, posicionBarcoJugador1, 2);
+                    mostrarCoordenadas(coordenadaJugador2, posBarco1, 2);
                     Console.WriteLine("JUGADOR 2");
                     Console.Write("Escoja una coordenada: \nRecuerda poner letra y numero");
-                    coordenadaElegidaJugador2[i, n] = Console.ReadLine();
-                    coordenadaElegidaJugador2[i, n] = coordenadaElegidaJugador2[i, n].ToUpper();
+                    coordenadaJugador2[i, n] = Console.ReadLine();
+                    coordenadaJugador2[i, n] = coordenadaJugador2[i, n].ToUpper();
                     Console.WriteLine("Cuando escribas tu coordenada presiona ENTER");
                     for (int a = 0; a < 3; a++)
                     {
-                        if (posicionBarcoJugador1[a] == coordenadaElegidaJugador2[i, n])
+                        if (posBarco1[a] == coordenadaJugador2[i, n])
                         {
                             Console.WriteLine("Le diste a un barco");
-                            vidasJugador1 = vidasJugador1 - 1;
+                            vidas1 = vidas1 - 1;
                             Console.ReadKey();
                         }
                     }
                     Console.Clear();
 
-                    if (vidasJugador1 == 0)
+                    if (vidas1 == 0)
                     {
                         Console.WriteLine("JUGADOR 2 GANO");
                         Console.ReadKey();
@@ -110,7 +107,6 @@ namespace BataallaNavalDAHT
                     }
                 }
             }
-
 
             Console.ReadKey();
         }
